f606ff31457b237f953fd98791560f1752b0d11c
Gabriel <gszcode@gmail.com>
2023-04-11 01:06:51 -0300

    config de variables de entorno

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST"),
-        srv: env.bool("DATABASE_SRV"),
-        port: env.int("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        host: env("DATABASE_HOST", "@cluster0.cbqftak.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "bar_strapi"),
+        username: env("DATABASE_USERNAME", "root"),
+        password: env("DATABASE_PASSWORD", "root"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", "admin"),
