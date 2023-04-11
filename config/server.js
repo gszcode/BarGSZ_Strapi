4d366a0ea1ab1198e5626aa44b650c043f83d302
Gabriel <gszcode@gmail.com>
2023-04-11 00:08:42 -0300

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '84e4640625dc94785e953fc308e3c81f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '42a3f51d-d295-4a2c-8404-3dc572e04be0'
+};
