50aeb04edebf9b99cec21a0a91ff20f053863c34
Gabriel <gszcode@gmail.com>
2023-04-25 00:14:10 -0300

    config realizadas

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "@cluster0.cbqftak.mongodb.net"),
+        host: env("DATABASE_HOST", "cluster0.cbqftak.mongodb.net"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
         database: env("DATABASE_NAME", "bar_strapi"),
